6b50a7b7567d6ec515e9d60d3770e3f8c9587b16
dreamdeveloperz <dreamdeveloperz@gmail.com>
2016-05-02 10:59:08 +0530

    new changes added
    convertImageToBase64FromUrl method is added  which will convert image in sd card to base64 string and return them

--- www/base64Image.js
+++ www/base64Image.js
@@ -3,7 +3,9 @@ var Base64Image = function () {
 Base64Image.prototype.saveImage = function (successCallback, errorCallback, base64String, params) {
     cordova.exec(successCallback, errorCallback, "Base64ImagePlugin", "saveImage", [base64String, params]);
 };
-
+Base64Image.prototype.convertImageToBase64FromUrl = function (successCallback, errorCallback, imageUrls, params) {
+    cordova.exec(successCallback, errorCallback, "Base64ImagePlugin", "convertImageToBase64FromUrl", [imageUrls, params]);
+};
 
 if (!window.plugins) {
     window.plugins = {};
